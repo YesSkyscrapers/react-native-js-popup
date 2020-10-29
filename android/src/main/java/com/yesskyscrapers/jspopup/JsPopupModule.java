60a6a994cb1e9d8607472d9f8e4d5833953e1c41
Pavel Nikolaev <yes.skyscrapers@gmail.com>
2020-10-29 16:26:35 +0300

    Смена бандл ид

--- android/src/main/java/com/yesskyscrapers/jspopup/JsPopupModule.java
+++ android/src/main/java/com/yesskyscrapers/jspopup/JsPopupModule.java
@@ -1,4 +1,4 @@
-package com.reactlibrary;
+package com.yesskyscrapers.jspopup;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
--- android/src/main/java/com/yesskyscrapers/jspopup/JsPopupPackage.java
+++ android/src/main/java/com/yesskyscrapers/jspopup/JsPopupPackage.java
@@ -1,4 +1,4 @@
-package com.reactlibrary;
+package com.yesskyscrapers.jspopup;
 
 import java.util.Arrays;
 import java.util.Collections;
